d7643373b5f8bfe68206637f68547c9aa6ebb9b6
PWLIMAVERDE <vorfast@gmail.com>
2019-09-21 19:19:33 -0300

    ajuste permissões

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,6 +1,6 @@
 from rest_framework import filters
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, DjangoModelPermissions
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, DjangoModelPermissions, IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
@@ -10,7 +10,7 @@ class PontoTuristicoViewSet(ModelViewSet):
     queryset = PontoTuristico.objects.all().order_by('-descricao')
     serializer_class = PontoTuristicoSerializer
     authentication_classes = (TokenAuthentication,)
-    #permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     filterset_fields = ['id', 'nome', 'descricao', 'aprovado']
     filter_backends = [filters.SearchFilter]
     search_fields = ['nome', 'descricao', 'aprovado']
--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['pontos-turisticos-pw.herokuapp.com', 'localhost:8000']
+ALLOWED_HOSTS = ['pontos-turisticos-pw.herokuapp.com', 'localhost']
 
 
 # Application definition
